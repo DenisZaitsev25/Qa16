58c0ab3e8296b28ea89a6083e8e8daf359191207
Denis <denis.zaitsev18@outlook.com>
2023-11-05 15:20:58 +0100

    save Lesson_1.py in git

---
+++ Lesson_1.py
@@ -0,0 +1,30 @@
+# task1
+# print(17/2*3+2)
+# print(2+17/2*3)
+# print(19%4+15/2*3)
+# print(15+6-10*4)
+# print(17/2%2*3**3)
+
+# task2
+# print(17/2*(3+2))
+# print((2+17)/2*3)
+# print(19%(4+15)/2*3)
+# print((15+6-10)*4)
+# print(17/2%(2*(3**3)))
+
+# task3
+# print(11-3*1.5)
+
+# task4
+# Anna_Apple = 2
+# Pol_Apple =5
+# print(Anna_Apple+Pol_Apple)
+
+# task5
+# days = 5; hours = 120; min = 7200; sec = 432000
+# print("Days:",days,"Hours:",hours,"Minute:",min,"Second:",sec)
+
+# task6
+# days1 = 182
+# week = 7
+# print(int(days1/week))
